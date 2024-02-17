1515a75c8ed5d4fa26abf1f4d5dac10fec17921d
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2024-02-18 01:59:49 +0500

    Todo

--- TestApp/src/todo.jsx
+++ TestApp/src/todo.jsx
@@ -1,35 +1,50 @@
 import { useState } from "react";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 const ToDo = () => {
-    const [list, setList] = useState([{"tast":"First",id:uuidv4()}])
+    const [list,setList]=useState([{vals:"First",id:uuidv4()}]);
     const [item,setItem]=useState("");
     const handleInput=(event)=>{
-        setItem(event.target.value);
+            setItem(event.target.value);
     }
-    const addTodo=()=>{
+    const add=()=>{
         setList((e)=>{
-            return [...e,{"tast":item}]
+            return [...list,{vals:item,id:uuidv4()}]
         })
-       
     }
+    const deleteVal=(id)=>{
+         let copy =list.filter((e)=>e.id!=id);
+         setList(copy)
+    }
+    const uppercase=()=>{
+        let upr=list.map((e)=>{
+           return {
+            ...list, vals:e.vals.toUpperCase()
+           }
+        })
+        setList(upr)
+    }
+
     return (
         <>
-            <h1>ToDoList</h1>
-
-            <input onChange={handleInput} type="text" />
-            <br />
-            <br />
-            <button onClick={addTodo}>Add</button>
-            <hr />
-            <ul>
-                {
-                    list.map((lst)=>(
-                        <li>
-                            {lst.tast}
-                        </li>
-                    ))
-                }
-            </ul>
+          <h1>ToDo List By Objectr</h1>
+          <br />
+          <input type="text" onChange={handleInput} />
+          <br />
+          <button onClick={add}>Add</button>
+          <hr />
+          <ul>
+            {
+                list.map((e)=>(
+                    <li key={e.id}>{e.vals}
+                    &nbsp;
+                    <button onClick={()=> deleteVal(e.id)}>Delete</button>
+                    </li>
+                    
+                ))
+            }
+          </ul>
+          <br />
+          <button onClick={uppercase}>Uppercase</button>
         </>
 
 
